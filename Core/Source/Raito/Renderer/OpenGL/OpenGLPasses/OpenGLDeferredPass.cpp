4a9f29e0b5ee27087318a4a978384a3da2c72c10
ZaroDev <victorfalcon796@gmail.com>
2024-06-02 22:34:19 +0200

    Update OpenGLDeferredPass.cpp

--- Core/Source/Raito/Renderer/OpenGL/OpenGLPasses/OpenGLDeferredPass.cpp
+++ Core/Source/Raito/Renderer/OpenGL/OpenGLPasses/OpenGLDeferredPass.cpp
@@ -178,7 +178,6 @@ namespace Raito::Renderer::OpenGL::Deferred
 		OPTICK_CATEGORY("Update Deferred", Optick::Category::Rendering);
 		// Geometry pass
 		{
-			g_FrameBuffer->Resize(buffer.Data().Width, buffer.Data().Height);
 			g_FrameBuffer->Bind();
 			glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 			glEnable(GL_DEPTH_TEST);
